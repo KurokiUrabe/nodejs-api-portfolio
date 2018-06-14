0fde62873db2e890c716ac63c13e69ab0a689480
Hello MH <arkangel.exe@gmail.com>
2018-06-14 11:18:31 -0500

    refactor conection

--- src/db/index.js
+++ src/db/index.js
@@ -3,8 +3,10 @@ import mongoose from "mongoose";
 import bluebird from "bluebird"; //better promises
 
 mongoose.Promise = bluebird; // replase promise
+const DB_CONECTION = process.env.DB_DEVELOPMENT;
 
-mongoose.connect(process.env.DB_DEVELOPMENT);
+mongoose.connect(DB_CONECTION);
+console.log("Database", DB_CONECTION);
 console.info("Mongoose state", mongoose.connection.readyState);
 
 const db = mongoose.connection;
