c12bbf46d6c7e8ec42f98b987bb8583a558586f2
Hello MH <arkangel.exe@gmail.com>
2018-06-20 16:59:17 -0500

    bug Promise name

--- src/db/index.js
+++ src/db/index.js
@@ -1,8 +1,9 @@
 import schema from "./schema";
 import mongoose from "mongoose";
-import bluebird from "bluebird"; //better promises
+import Promise from "bluebird"; //better promises
 
-mongoose.Promise = bluebird; // replase promise
+Promise.promisifyAll(mongoose);
+mongoose.Promise = Promise; // replase promise
 const DB_CONECTION = process.env.DB_DEVELOPMENT;
 
 mongoose.connect(DB_CONECTION);
